c65790f39fa04e649d6835746fa411f509b06714
Víctor González <victor.gonzalez.haya.contractor@bbva.com>
2023-06-27 13:41:39 +0200

    feat: github pages domain config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import {
+  createBrowserRouter,
   Route,
-  Routes
+  RouterProvider,
+  Routes,
 } from "react-router-dom";
 
 import { Home } from './views/Home.js';
 import { Profile } from './views/Profile.js';
 import { Product } from './views/Product.js';
 
-// 1️⃣ Changed from App to Root
+const router = createBrowserRouter(
+  [{ path: "*", Component: Root }],
+  { basename: "/u-app" }
+);
+
+// 4️⃣ RouterProvider added
 function App() {
+  return <RouterProvider router={router} />;
+}
+
+// 1️⃣ Changed from App to Root
+function Root() {
   // 2️⃣ `BrowserRouter` component removed, but the <Routes>/<Route>
   // component below are unchanged
   return (
